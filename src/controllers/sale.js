72c3e9f8ba0fef3908a459de416385c7a12ee6c4
Habip Hakkioglu <h.hakkioglu@icloud.com>
2023-11-26 18:34:25 +0100

    controllers written

--- src/controllers/sale.js
+++ src/controllers/sale.js
@@ -1,4 +1,104 @@
 "use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
+
+// Sale Controller:
+
+const Sale = require('../models/sale')
+
+module.exports={
+    list: async (req, res) => {
+        /*
+            #swagger.tags = ["Products"]
+            #swagger.summary = "List Sales"
+            #swagger.description = `
+                You can send query with endpoint for search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
+            `
+        */
+        
+        const data = await res.getModelList(Sale)   //find yerine bunu yapiyoruz cünkü pagination sayfasindaki search sort gibi seylerin aktif olabilmesi icin getModelList kullaniyoruz.
+
+        res.status(200).send({
+            error: false,
+            detail: await res.getModelListDetails(Sale),
+            data
+        })
+    },
+    create: async (req, res) => {
+
+        /*
+            #swagger.tags = ["Sales"]
+            #swagger.summary = "Create Sale"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: { }
+            }
+        */
+
+        const data = await Sale.create(req.body)
+
+        res.status(201).send({
+            error: false,
+            data
+        })
+    },
+    read: async (req, res) => {
+
+        /*
+            #swagger.tags = ["Sales"]
+            #swagger.summary = "Get Single Sale"
+        */
+        const data = await Sale.findOne({_id: req.params.id})
+
+        res.status(200).send({
+            error: false,
+            data
+        })
+    },
+    update: async (req, res) => {
+
+        /*
+            #swagger.tags = ["Sales"]
+            #swagger.summary = "Update Sale"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                }
+            }
+        */
+
+        const data = await Sale.updateOne({_id: req.params.id}, req.body, {runValidators: true})
+
+        res.status(202).send({
+            error: false,
+            data,
+            new: await Sale.findOne({_id: req.params.id})
+        })
+    },
+    delete: async (req, res) => {
+
+        /*
+            #swagger.tags = ["Sales"]
+            #swagger.summary = "Delete Sale"
+        */
+        const data = await Sale.deleteOne({_id: req.params.id})
+
+        res.status(data.deletedCount ? 202 : 404).send({
+            error: false,
+            data
+        })
+    },
+}
+
+
+
+
+
+
+
+
